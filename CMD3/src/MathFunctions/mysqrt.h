079dae73430406185d14719ebccc0adf15799dd6
Tony Tan <Tony@TonydeMacBook-Pro.local>
2016-09-28 18:18:56 +0800

    init study cmake project

---
+++ CMD3/src/MathFunctions/mysqrt.h
@@ -0,0 +1,4 @@
+#ifndef MATHFUNCTIONS_MYSQRT_H
+#define MATHFUNCTIONS_MYSQRT_H
+double mysqrt(double);
+#endif
---
+++ CMD4/src/MathFunctions/mysqrt.cxx
@@ -0,0 +1,11 @@
+//A simple program that computes the square root of a number
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double mysqrt(double inputValue)
+{
+	double outputValue= sqrt(inputValue);
+	return outputValue;
+}
